a043840de03bf25408fa47234e2cb8deeb77ab74
Germán De Francesco <germandefrancesco@hotmail.com>
2023-05-06 12:42:40 -0300

    Add Migrations, connect layers, fix model nullables and apply migrations on startup in Ordering service

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,5 +1,12 @@
+using Ordering.Api.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,5 +17,11 @@ app.UseSwagger();
 app.UseSwaggerUI();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetRequiredService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
 
 app.Run();
--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -11,11 +11,11 @@ public class Order : EntityBase
     public required string EmailAddress { get; set; }
     public required string AddressLine { get; set; }
     public required string Country { get; set; }
-    public string State { get; set; }
-    public string ZipCode { get; set; }
-    public string CardName { get; set; }
-    public string CardNumber { get; set; }
-    public string Expiration { get; set; }
-    public string CVV { get; set; }
-    public int PaymentMethod { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
+    public string? CardName { get; set; }
+    public string? CardNumber { get; set; }
+    public string? Expiration { get; set; }
+    public string? CVV { get; set; }
+    public int? PaymentMethod { get; set; }
 }
