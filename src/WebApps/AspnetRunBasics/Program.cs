e582fbd29897bc93afabd67a5b509cca89022307
Germán De Francesco <germandefrancesco@hotmail.com>
2023-05-16 20:16:36 -0300

    Remove all unnecessary code and nuget packages in AspnetRunBasics project

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,16 +1,4 @@
-using AspnetRunBasics.Data;
-using AspnetRunBasics.Repositories;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<AspnetRunContext>(x =>
-    x.UseSqlServer(builder.Configuration.GetConnectionString("AspnetRunConnection")));
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
 builder.Services.AddRazorPages();
 
@@ -22,6 +10,5 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
-app.SeedDatabase();
 
 app.Run();
