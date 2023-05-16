a015ad41b8ff079858123e2f3f4ed19304c75697
Germán De Francesco <germandefrancesco@hotmail.com>
2023-05-15 22:43:48 -0300

    Add Catalog, Basket and Order services, fix appsettings urls and configure httpclients in Shopping.Aggregator project

---
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions;
+
+public static class HttpClientExtensions
+{
+    public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+    {
+        if (response.IsSuccessStatusCode is false)
+        {
+            throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+        }
+
+        var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        var deserializedObject = JsonSerializer.Deserialize<T>(dataAsString, 
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        if (deserializedObject is null)
+        {
+            throw new ApplicationException($"Something went wrong deserializing the string: {dataAsString} as {typeof(T).Name}");
+        }
+
+        return deserializedObject;    
+    }
+}
--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,4 +1,18 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"] ?? 
+        throw new ArgumentNullException(nameof(builder.Configuration), "Configuration missing")));
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"] ??
+        throw new ArgumentNullException(nameof(builder.Configuration), "Configuration missing")));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(uriString: builder.Configuration["ApiSettings:OrderingUrl"] ??
+        throw new ArgumentNullException(nameof(builder.Configuration), "Configuration missing")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
